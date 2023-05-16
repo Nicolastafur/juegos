9cadb34d23c49d01420b682f7c944e37ae6235d6
Nicolas 123 <100376064+Nicolastafur@users.noreply.github.com>
2023-05-16 10:17:26 -0500

    Add files via upload

--- juegos/README.md
+++ juegos/README.md
@@ -1,7 +1,41 @@
-This is a complete well automated chess game made by object oriented javascript I hope you like it.
+# tetris-javascript
+ Tetris escrito con JavaScript. Se usa canvas para dibujar el tablero.
+ 
+ ![Parzibyte jugando Tetris programado con JavaScript](https://parzibyte.me/blog/wp-content/uploads/2020/11/Jugando-Tetris-en-JavaScript-juego-open-source.png)
+ 
+ **By parzibyte**
+ 
+ **Tutorial**: https://parzibyte.me/blog/2020/11/02/tetris-javascript-open-source/
+ 
+ **Demo**: https://parzibyte.github.io/tetris-javascript/
+ 
 
-Play it from here: https://ahmadalkholy.github.io/Javascript-Chess-Game/chess.html
+# Documentación del estilo de código
+Preferimos algo legible a algo "optimizado". Por ejemplo, en lugar de:
+```javascript
+return !tablero[y][x].ocupado;
+```
+Se prefiere:
+```javascript
+if (tablero[y][x].ocupado){
+    return false
+}else {
+    return true;
+}
+```
 
-------------
+# Docs
 
-The chess pieces images are originaly made by <a href="https://pixabay.com/users/Clker-Free-Vector-Images-3736/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=26774" target="_blank">Clker-Free-Vector-Images</a> from <a href="https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=26774" target="_blank">Pixabay</a> you can get get it from <a href="https://pixabay.com/vectors/chess-pieces-set-symbols-game-26774/" target="_blank">here</a>.
+**Absolute point**: A point with x and y that is absolute to the game board
+
+**Relative point**: A point with inner x and y; for example, a point that conforms a figure
+
+# Files
+**script.js**: just a legacy code file. It is not necessary at all,
+it's just to remember the early development of this port
+
+**tetris.js**: the main file. It contains 3 classes. If you want, you
+can separate all of them; they are independent.
+
+# About
+Proudly brought to you by parzibyte (https://parzibyte.me/blog)
--- juegos/js/tetris.js
+++ juegos/js/tetris.js
@@ -1,242 +1,298 @@
-const ROW = 20;
-const COLUMN = 10;
-
-let canvasBoard = document.getElementById('board-canvas');
-let contextBoard = canvasBoard.getContext('2d');
-let canvasPiece = document.getElementById('piece-canvas');
-let contextPiece = canvasPiece.getContext('2d');
-let scoreElement = document.getElementById('score');
-let intervalId;
-let board;
-let currentPiece;
-let currentPiecePosition;
-let score = 0;
-
-function createBoard() {
-  board = new Array(ROW).fill(null).map(() => new Array(COLUMN).fill(null));
-}
-
-function drawBoard() {
-  contextBoard.clearRect(0, 0, canvasBoard.width, canvasBoard.height);
-  board.forEach((row, r) => {
-    row.forEach((value, c) => {
-      if (value !== null) {
-        contextBoard.fillStyle = getColor(value);
-        contextBoard.fillRect(c * 20, r * 20, 20, 20);
-      }
-    });
-  });
-}
-
-function drawPiece() {
-  contextPiece.clearRect(0, 0, canvasPiece.width, canvasPiece.height);
-  currentPiece.forEach((row, r) => {
-    row.forEach((value, c) => {
-      if (value) {
-        contextPiece.fillStyle = getColor(value);
-        contextPiece.fillRect(
-          (currentPiecePosition.x + c) * 20,
-          (currentPiecePosition.y + r) * 20,
-          20,
-          20
-        );
-      }
-    });
-  });
-}
-
-function getColor(value) {
-  switch (value) {
-    case 1:
-      return '#FF0D72';
-    case 2:
-      return '#0DC2FF';
-    case 3:
-      return '#0DFF72';
-    case 4:
-      return '#F538FF';
-    case 5:
-      return '#FF8E0D';
-    case 6:
-      return '#FFE138';
-    case 7:
-      return '#3877FF';
-    default:
-      return '#000';
-  }
-}
-
-function getRandomPiece() {
-  const pieces = [
-    [
-      [1, 1],
-      [1, 1],
-    ],
-    [
-      [2, 2, 0],
-      [0, 2, 2],
-      [0, 0, 0],
-    ],
-    [
-      [0, 3, 3],
-      [3, 3, 0],
-      [0, 0, 0],
-    ],
-    [
-      [4, 4, 0],
-      [0, 4, 4],
-      [0, 0, 0],
-    ],
-    [
-      [0, 5, 0],
-      [5, 5, 5],
-      [0, 0, 0],
-    ],
-    [
-      [6, 6, 6, 6],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-    ],
-    [
-      [0, 0, 7],
-      [7, 7, 7],
-      [0, 0, 0],
-    ],
-  ];
-
-  const randomIndex = Math.floor(Math.random() * pieces.length);
-  return pieces[randomIndex];
-}
-
-function canMove(piece, position) {
-    return piece.every((row, r) => {
-      return row.every((value, c) => {
-        const rowPosition = position.y + r;
-        const colPosition = position.x + c;
-        if (value === 0)       return true;
-        if (
-          rowPosition < 0 ||
-          rowPosition >= ROW ||
-          colPosition < 0 ||
-          colPosition >= COLUMN ||
-          board[rowPosition][colPosition] !== null
-        ) {
-          return false;
-        }
-        return true;
-      });
-    });
-  }
-  
-  function mergePiece() {
-    currentPiece.forEach((row, r) => {
-      row.forEach((value, c) => {
-        if (value) {
-          const rowPosition = currentPiecePosition.y + r;
-          const colPosition = currentPiecePosition.x + c;
-          board[rowPosition][colPosition] = value;
-        }
-      });
-    });
-  }
-  
-  function checkLines() {
-    let lines = 0;
-    board.forEach((row, r) => {
-      if (row.every((value) => value !== null)) {
-        lines++;
-        board.splice(r, 1);
-        board.unshift(new Array(COLUMN).fill(null));
-      }
-    });
-  
-    score += lines * 10;
-    scoreElement.innerHTML = score;
-  }
-  
-  function gameover() {
-    clearInterval(intervalId);
-    alert('Game Over!');
-  }
-  
-  function dropPiece() {
-    const newPosition = {
-      ...currentPiecePosition,
-      y: currentPiecePosition.y + 1,
+// https://tetris.fandom.com/wiki/Tetris_Guideline
+// get a random integer between the range of [min,max]
+// @see https://stackoverflow.com/a/1527820/2124254
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    
+    // generate a new tetromino sequence
+    // @see https://tetris.fandom.com/wiki/Random_Generator
+    function generateSequence() {
+    const sequence = ['I', 'J', 'L', 'O', 'S', 'T', 'Z'];
+    
+    while (sequence.length) {
+    const rand = getRandomInt(0, sequence.length - 1);
+    const name = sequence.splice(rand, 1)[0];
+    tetrominoSequence.push(name);
+    }
+    }
+    
+    // get the next tetromino in the sequence
+    function getNextTetromino() {
+    if (tetrominoSequence.length === 0) {
+    generateSequence();
+    }
+    
+    const name = tetrominoSequence.pop();
+    const matrix = tetrominos[name];
+    
+    // I and O start centered, all others start in left-middle
+    const col = playfield[0].length / 2 - Math.ceil(matrix[0].length / 2);
+    
+    // I starts on row 21 (-1), all others start on row 22 (-2)
+    const row = name === 'I' ? -1 : -2;
+    
+    return {
+    name: name, // name of the piece (L, O, etc.)
+    matrix: matrix, // the current rotation matrix
+    row: row, // current row (starts offscreen)
+    col: col // current col
     };
-  
-    if (canMove(currentPiece, newPosition)) {
-      currentPiecePosition = newPosition;
-    } else {
-      mergePiece();
-      checkLines();
-      currentPiece = getRandomPiece();
-      currentPiecePosition = { x: 3, y: 0 };
-  
-      if (!canMove(currentPiece, currentPiecePosition)) {
-        gameover();
-        return;
+    }
+    
+    // rotate an NxN matrix 90deg
+    // @see https://codereview.stackexchange.com/a/186834
+    function rotate(matrix) {
+    const N = matrix.length - 1;
+    const result = matrix.map((row, i) =>
+    row.map((val, j) => matrix[N - j][i])
+    );
+    
+    return result;
+    }
+    
+    // check to see if the new matrix/row/col is valid
+    function isValidMove(matrix, cellRow, cellCol) {
+    for (let row = 0; row < matrix.length; row++) {
+    for (let col = 0; col < matrix[row].length; col++) {
+    if (matrix[row][col] && (
+    // outside the game bounds
+    cellCol + col < 0 ||
+    cellCol + col >= playfield[0].length ||
+    cellRow + row >= playfield.length ||
+    // collides with another piece
+    playfield[cellRow + row][cellCol + col])
+    ) {
+    return false;
+    }
+    }
+    }
+    
+    return true;
+    }
+    
+    // place the tetromino on the playfield
+    function placeTetromino() {
+    for (let row = 0; row < tetromino.matrix.length; row++) {
+    for (let col = 0; col < tetromino.matrix[row].length; col++) {
+    if (tetromino.matrix[row][col]) {
+    
+    Copy
+        // game over if piece has any part offscreen
+        if (tetromino.row + row < 0) {
+          return showGameOver();
+        }
+    
+        playfield[tetromino.row + row][tetromino.col + col] = tetromino.name;
       }
     }
-  
-    drawBoard();
-    drawPiece();
-  }
-  
-  function startGame() {
-    createBoard();
-    drawBoard();
-  
-    currentPiece = getRandomPiece();
-    currentPiecePosition = { x: 3, y: 0 };
-    drawPiece();
-  
-    intervalId = setInterval(() => {
-      dropPiece();
-    }, 1000);
-  
-    document.addEventListener('keydown', (event) => {
-      if (event.keyCode === 37) {
-        const newPosition = {
-          ...currentPiecePosition,
-          x: currentPiecePosition.x - 1,
-        };
-        if (canMove(currentPiece, newPosition)) {
-          currentPiecePosition = newPosition;
-          drawPiece();
-        }
-      } else if (event.keyCode === 39) {
-        const newPosition = {
-          ...currentPiecePosition,
-          x: currentPiecePosition.x + 1,
-        };
-        if (canMove(currentPiece, newPosition)) {
-          currentPiecePosition = newPosition;
-          drawPiece();
-        }
-      } else if (event.keyCode === 40) {
-        dropPiece();
-      } else if (event.keyCode === 38) {
-        const rotatedPiece = currentPiece[0].map((val, index) =>
-          currentPiece.map((row) => row[index]).reverse()
-        );
-        if (canMove(rotatedPiece, currentPiecePosition)) {
-          currentPiece = rotatedPiece;
-          drawPiece();
+    }
+    
+    // check for line clears starting from the bottom and working our way up
+    for (let row = playfield.length - 1; row >= 0; ) {
+    if (playfield[row].every(cell => !!cell)) {
+    
+    ini
+    Copy
+      // drop every row above this one
+      for (let r = row; r >= 0; r--) {
+        for (let c = 0; c < playfield[r].length; c++) {
+          playfield[r][c] = playfield[r-1][c];
         }
       }
+    }
+    else {
+      row--;
+    }
+    }
+    
+    tetromino = getNextTetromino();
+    }
+    
+    // show the game over screen
+    function showGameOver() {
+    cancelAnimationFrame(rAF);
+    gameOver = true;
+    
+    context.fillStyle = 'black';
+    context.globalAlpha = 0.75;
+    context.fillRect(0, canvas.height / 2 - 30, canvas.width, 60);
+    
+    context.globalAlpha = 1;
+    context.fillStyle = 'white';
+    context.font = '36px monospace';
+    context.textAlign = 'center';
+    context.textBaseline = 'middle';
+    context.fillText('GAME OVER!', canvas.width / 2, canvas.height / 2);
+    }
+    
+    const canvas = document.getElementById('game');
+    const context = canvas.getContext('2d');
+    const grid = 32;
+    const tetrominoSequence = [];
+    
+    // keep track of what is in every cell of the game using a 2d array
+    // tetris playfield is 10x20, with a few rows offscreen
+    const playfield = [];
+    
+    // populate the empty state
+    for (let row = -2; row < 20; row++) {
+    playfield[row] = [];
+    
+    for (let col = 0; col < 10; col++) {
+    playfield[row][col] = 0;
+    }
+    }
+    
+    // how to draw each tetromino
+    // @see https://tetris.fandom.com/wiki/SRS
+    const tetrominos = {
+    'I': [
+    [0,0,0,0],
+    [1,1,1,1],
+    [0,0,0,0],
+    [0,0,0,0]
+    ],
+    'J': [
+    [1,0,0],
+    [1,1,1],
+    [0,0,0],
+    ],
+    'L': [
+    [0,0,1],
+    [1,1,1],
+    [0,0,0],
+    ],
+    'O': [
+    [1,1],
+    [1,1],
+    ],
+    'S': [
+    [0,1,1],
+    [1,1,0],
+    [0,0,0],
+    ],
+    'Z': [
+    [1,1,0],
+    [0,1,1],
+    [0,0,0],
+    ],
+    'T': [
+    [0,1,0],
+    [1,1,1],
+    [0,0,0],
+    ]
+    };
+    
+    // color of each tetromino
+    const colors = {
+    'I': 'cyan',
+    'O': 'yellow',
+    'T': 'purple',
+    'S': 'green',
+    'Z': 'red',
+    'J': 'blue',
+    'L': 'orange'
+    };
+    
+    let count = 0;
+    let tetromino = getNextTetromino();
+    let rAF = null; // keep track of the animation frame so we can cancel it
+    let gameOver = false;
+    
+    // game loop
+    function loop() {
+    rAF = requestAnimationFrame(loop);
+    context.clearRect(0,0,canvas.width,canvas.height);
+    
+    // draw the playfield
+    for (let row = 0; row < 20; row++) {
+    for (let col = 0; col < 10; col++) {
+    if (playfield[row][col]) {
+    const name = playfield[row][col];
+    context.fillStyle = colors[name];
+    
+    reasonml
+    Copy
+        // drawing 1 px smaller than the grid creates a grid effect
+        context.fillRect(col * grid, row * grid, grid-1, grid-1);
+      }
+    }
+    }
+    
+    // draw the active tetromino
+    if (tetromino) {
+    
+    maxima
+    Copy
+    // tetromino falls every 35 frames
+    if (++count > 35) {
+      tetromino.row++;
+      count = 0;
+    
+      // place piece if it runs into anything
+      if (!isValidMove(tetromino.matrix, tetromino.row, tetromino.col)) {
+        tetromino.row--;
+        placeTetromino();
+      }
+    }
+    
+    context.fillStyle = colors[tetromino.name];
+    
+    for (let row = 0; row < tetromino.matrix.length; row++) {
+      for (let col = 0; col < tetromino.matrix[row].length; col++) {
+        if (tetromino.matrix[row][col]) {
+    
+          // drawing 1 px smaller than the grid creates a grid effect
+          context.fillRect((tetromino.col + col) * grid, (tetromino.row + row) * grid, grid-1, grid-1);
+        }
+      }
+    }
+    }
+    }
+    
+    // listen to keyboard events to move the active tetromino
+    document.addEventListener('keydown', function(e) {
+    if (gameOver) return;
+    
+    // left and right arrow keys (move)
+    if (e.which === 37 || e.which === 39) {
+    const col = e.which === 37
+    ? tetromino.col - 1
+    : tetromino.col + 1;
+    
+    Copy
+    if (isValidMove(tetromino.matrix, tetromino.row, col)) {
+      tetromino.col = col;
+    }
+    }
+    
+    // up arrow key (rotate)
+    if (e.which === 38) {
+    const matrix = rotate(tetromino.matrix);
+    if (isValidMove(matrix, tetromino.row, tetromino.col)) {
+    tetromino.matrix = matrix;
+    }
+    }
+    
+    // down arrow key (drop)
+    if(e.which === 40) {
+    const row = tetromino.row + 1;
+    
+    Copy
+    if (!isValidMove(tetromino.matrix, row, tetromino.col)) {
+      tetromino.row = row - 1;
+    
+      placeTetromino();
+      return;
+    }
+    
+    tetromino.row = row;
+    }
     });
-  }
-  
-  function stopGame() {
-    clearInterval(intervalId);
-  }
-  
-  let startButton = document.getElementById('start-button');
-  startButton.addEventListener('click', startGame);
-  
-  let stopButton = document.getElementById('stop-button');
-  stopButton.addEventListener('click', stopGame);
-  
-          
+    
+    // start the game
+    rAF = requestAnimationFrame(loop);
